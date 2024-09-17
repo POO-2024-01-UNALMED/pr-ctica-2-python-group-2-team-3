d573858a9f741b64ca113b5c6a59d7ffb7a3e5a3
Paulipupo <mpupom@unal.edu.co>
2024-09-16 19:03:25 -0500

    arreglar importaciones

--- src/interfazDeUsuario/ventanaPrincipalDeUsuario.py
+++ src/interfazDeUsuario/ventanaPrincipalDeUsuario.py
@@ -10,7 +10,17 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'modulos
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), 'gestorAplicacion')))
 directorio_actual = os.path.dirname(os.path.abspath(__file__))
 # Importar clases 
-from src.interfazDeUsuario import *
+from clases.menu import Menu
+from clases.pdfViewer import PdfViewer
+from clases.fieldFrame import FieldFrame
+from clases.tabla import Tabla
+
+# Importar modulos
+from modulos.verOpcionesDeAdministrador import *
+#from modulos.reservarActividades import *
+#from modulos.reservarHospedaje import *
+from modulos.planearViaje import *
+from modulos.modificarReserva import *
 
 
 
